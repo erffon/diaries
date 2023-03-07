247f0f967d89ad93310c26358f666b21e8af2bc5
office <erffon@gmail.com>
2023-03-07 16:45:50 +0330

    FIX : project setups
    - tailwind installation and configuration
    - favicon and font configuration

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./features/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        salel: ["SAHEL", "Helvetica", "Arial", "sans-serif"],
+      },
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+  },
+  plugins: [],
+};
