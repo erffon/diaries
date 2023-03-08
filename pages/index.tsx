8a28f5e925158583deab52ecedf20176a3cecb78
office <erffon@gmail.com>
2023-03-09 01:15:24 +0330

    FEAT : Mock data and Recent articles
    - homepage recent articles design
    - mock data has been added to test the app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
 import Header from "@/features/Header";
-
-const inter = Inter({ subsets: ["latin"] });
+import ARTICLES from "../mockdata/articles";
 
 export default function Home() {
   return (
@@ -14,9 +12,34 @@ export default function Home() {
         <link rel="icon" href="/myfavicon.ico" />
       </Head>
       <Header />
-      <main className="mx-10">
-        <div className="w-auto h-screen"></div>
-      </main>
+      <div className="mx-10 w-auto pb-16 flex justify-between">
+        <main className=" w-3/5">
+          <h2 className="font-bold text-slate-500 rounded-lg mb-5 py-3 px-3 bg-indigo-100">
+            آخرین نوشته‌ها
+          </h2>
+          <ul>
+            {ARTICLES.slice(0, 3).map((item) => {
+              return (
+                <li
+                  key={item.id}
+                  className="border-b pb-7 px-3 hover:scale-105 transition duration-300 ease-out hover:cursor-pointer"
+                >
+                  <h3 className="font-semibold my-2 text-neutral-500">
+                    {item.title}
+                  </h3>
+                  <p className="mb-3 text-neutral-400 font-light text-sm">
+                    {item.body.substring(0, 200)}
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
+          <button className="bg-slate-200 px-3 py-1.5 mt-3 text-gray-600 rounded-lg hover:scale-95 transition duration-200 ease-out text-sm">
+            مشاهده همه
+          </button>
+        </main>
+        <aside className="w-1/3 h-2/3"></aside>
+      </div>
     </>
   );
 }
