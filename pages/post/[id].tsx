ce43359134b9ddb1f5bbd32c2a5472e5ed1a6837
office <erffon@gmail.com>
2023-03-18 02:10:16 +0330

    FIX : release time
    - release time fixed
    - tag and release time design enhanced

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { apiRoutes } from "@/constants/api-routes";
 import { PostsType } from "@/interfaces/posts";
 import MarkdownIt from "markdown-it";
+import { BiPurchaseTagAlt } from "react-icons/bi";
 
 interface PostData {
   id: number;
@@ -18,18 +19,45 @@ interface PostData {
     publishedAt: Date;
   };
 }
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 const Article = ({ postData }: { postData: PostData }) => {
   const md = new MarkdownIt();
   const postDescription = md.render(postData?.attributes?.description);
+
+  const postRawTime = postData?.attributes?.updatedAt.toString();
+  const time = new Date(postRawTime);
+  const releaseTime = `${time.getDay()} ${monthNames[time.getMonth()].slice(
+    0,
+    3
+  )} ${time.getFullYear()}`;
+
   return (
     <>
       <Header />
       <main className="font-salel h-screen">
         <h2 className={style.title}>{postData?.attributes?.title}</h2>
         <div className={style["post-metadata"]}>
-          <button>{postData?.attributes?.tag}</button>
-          <p>{postData?.attributes?.updatedAt.toString()}</p>
+          <button className="flex items-center gap-2 bg-neutral-100 px-3 py-1 rounded-lg text-sm text-neutral-400">
+            <BiPurchaseTagAlt />
+            {postData?.attributes?.tag}
+          </button>
+          <p className="text-neutral-400 text-sm bg-neutral-100 px-3 py-1 rounded-lg">
+            {releaseTime}
+          </p>
         </div>
         <p
           className={style.description}
