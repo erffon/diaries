c0aad6bf1512c7234862a15c04602d7a0de895b0
office <erffon@gmail.com>
2023-03-25 00:54:59 +0430

    FEAT : search added
    - search bar has been added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,15 @@ import axios from "axios";
 import Head from "next/head";
 import Header from "@/features/Header";
 import { HiFire } from "react-icons/hi";
-import { BiPurchaseTagAlt } from "react-icons/bi";
+import { BiPurchaseTagAlt, BiSearch } from "react-icons/bi";
 import style from "./Homepage.module.css";
 import { apiRoutes } from "@/constants/api-routes";
-import { PostsType } from "@/interfaces/posts";
+import { Datum, PostsType } from "@/interfaces/posts";
 import Link from "next/link";
 import { Routes } from "@/constants/app-routes";
 import { AllCatType } from "@/interfaces/categories";
 import { TbCategory2 } from "react-icons/tb";
+import { ChangeEvent, EventHandler, useState } from "react";
 
 export default function Home({
   data: { data: POSTS },
@@ -21,6 +22,15 @@ export default function Home({
   status: any;
   catData: AllCatType;
 }) {
+  const [searched, setSearched] = useState<Datum[]>([]);
+
+  const searchHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+    const searchResult = POSTS.filter((item) =>
+      item?.attributes?.title?.includes(query)
+    );
+    setSearched(searchResult);
+  };
   return (
     <>
       <Head>
@@ -30,6 +40,17 @@ export default function Home({
         <link rel="icon" href="/myfavicon.ico" />
       </Head>
       <Header />
+      <div className="-mt-2 mx-10">
+        <div className="pb-5 flex items-center">
+          <input
+            type="text"
+            placeholder="جستجو ..."
+            className="py-2 px-5 rounded-r-lg"
+            onChange={searchHandler}
+          />
+          <BiSearch className="text-white bg-slate-600 w-10 h-10 flex justify-center p-2 rounded-l-lg" />
+        </div>
+      </div>
       <div className="mx-10 w-auto pb-16 flex justify-between">
         <main className=" w-3/5">
           <h2 className="font-bold text-slate-500 rounded-lg mb-5 py-3 px-3 bg-indigo-100 flex gap-2 items-center">
@@ -39,7 +60,8 @@ export default function Home({
           </h2>
           <ul>
             {status === 200 &&
-              POSTS.reverse()
+              searched
+                .reverse()
                 .slice(0, 3)
                 .map((item) => {
                   return (
